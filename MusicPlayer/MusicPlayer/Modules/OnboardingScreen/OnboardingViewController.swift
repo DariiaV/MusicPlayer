c5469a02e71391165a4cdebe81d014ec9f756b82
Dariia <dariia.grigoreva@gmail.com>
2023-01-08 20:11:03 +0400

    create onboarding screen

---
+++ MusicPlayer/MusicPlayer/Modules/OnboardingScreen/OnboardingViewController.swift
@@ -0,0 +1,107 @@
+//
+//  OnboardingFirstViewController.swift
+//  MusicPlayer
+//
+//  Created by Дария Григорьева on 08.01.2023.
+//
+
+import UIKit
+class OnboardingViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "OnboardingFirst")
+        return imageView
+    }()
+    
+    private let welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Welcome to the world of music"
+        label.font = UIFont(name: "Marker Felt", size: 24)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        label.textColor = .white
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "We are a small boutique music venue in the heart of..."
+        label.font = UIFont(name: "Marker Felt", size: 16)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        label.textColor = .white
+        return label
+    }()
+    
+    private lazy var nextButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Next", for: .normal)
+        button.addTarget(self, action: #selector(goNextStep(_:)), for: .touchUpInside)
+        button.backgroundColor = .cyan
+        return button
+    }()
+    private var isNextButtonTapped = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .black
+        
+        setupView()
+        setupNavigationItem()
+    }
+    
+    private func setupView() {
+        view.addSubviews([imageView, welcomeLabel, descriptionLabel, nextButton])
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 225),
+            imageView.heightAnchor.constraint(equalToConstant: 207),
+            imageView.widthAnchor.constraint(equalToConstant: 207),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            welcomeLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 24),
+            welcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 80),
+            welcomeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -80),
+            welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 8),
+            descriptionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            nextButton.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 49),
+            nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -29),
+            nextButton.widthAnchor.constraint(equalToConstant: 66),
+            nextButton.heightAnchor.constraint(equalToConstant: 40)
+            
+        ])
+    }
+    
+    private func setupNavigationItem() {
+        let rightItem = UIBarButtonItem(title: "Skip",
+                                        image: nil,
+                                        target: self,
+                                        action: #selector(skipOnboarding))
+        rightItem.tintColor = .white
+        navigationItem.rightBarButtonItem = rightItem
+    }
+    
+    @objc private func skipOnboarding() {
+        // next screen
+        dismiss(animated: true)
+    }
+    
+    @objc private func goNextStep(_ sender: UIButton) {
+        if isNextButtonTapped {
+            skipOnboarding()
+        } else {
+            imageView.image = UIImage(named: "OnboardingSecond")
+            nextButton.setTitle("Start", for: .normal)
+            welcomeLabel.text = "Download and save your Favourit Music"
+            descriptionLabel.text = "Your best music is always with you 🎧"
+            navigationItem.rightBarButtonItem?.isHidden = true
+            isNextButtonTapped = true
+        }
+    }
+    
+}
