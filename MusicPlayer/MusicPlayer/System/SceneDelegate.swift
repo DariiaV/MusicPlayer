639d969ab8358dd7f6bfa00432ded0265f48d444
Artem Galay <iostema@mail.ru>
2023-01-19 20:43:55 +0300

    Final version.
    # Conflicts:
    #	MusicPlayer/MusicPlayer.xcodeproj/project.pbxproj
    #	MusicPlayer/MusicPlayer/System/SceneDelegate.swift

--- MusicPlayer/MusicPlayer/System/SceneDelegate.swift
+++ MusicPlayer/MusicPlayer/System/SceneDelegate.swift
@@ -7,21 +7,36 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let scene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: scene)
-        let viewController = MainScreenViewController()
-        let navigationController = TabBarController()
-        window?.rootViewController = navigationController
-        
-        window?.makeKeyAndVisible()
+        self.setupWindow(with: scene)
+        self.checkAuthentication()
+    }
+
+    private func setupWindow(with scene: UIScene) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        self.window?.makeKeyAndVisible()
         FirebaseApp.configure()
     }
-    
-    
+
+    public func checkAuthentication() {
+        if Auth.auth().currentUser == nil {
+            goToController(with: RegistrationViewController())
+        } else {
+            goToController(with: TabBarController())
+        }
+    }
+
+    private func goToController(with viewController: UIViewController) {
+        let nav = UINavigationController(rootViewController: viewController)
+        nav.modalPresentationStyle = .fullScreen
+        window?.rootViewController = nav
+    }
 }
